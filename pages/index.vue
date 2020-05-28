60745347afb150785cc57c9e1839612722c97c14
Shai Gerbi <ShaiGerbi@gmail.com>
2020-05-28 11:53:12 +0300

    Insert users

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-
+    <p v-for="user in users" :key="user.id">{{ user.name }}</p>
   </div>
 </template>
 
 <script>
+import User from '@/models/User'
 export default {
+  created() {
+    User.insert({data: {id: 1, name: 'Shai'}})
+    User.insert({data: {id: 2, name: 'Maya'}})
+  },
 
+  computed:{
+    users(){
+      return User.all()
+    }
+  }
 }
 </script>
