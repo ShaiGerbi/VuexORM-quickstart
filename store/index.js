7b44be0578a300e769fb632f5cf25bd3bcd3c1c7
Shai Gerbi <ShaiGerbi@gmail.com>
2020-05-28 16:28:34 +0300

    Setup vuex-orm-axios

--- store/index.js
+++ store/index.js
@@ -1,5 +1,13 @@
+import Vuex from 'vuex'
 import VuexORM from '@vuex-orm/core'
-import database from '@/database'
+import VuexORMAxios from '@vuex-orm/plugin-axios'
+import User from '@/models/User'
+
+VuexORM.use(VuexORMAxios)
+
+const database = new VuexORM.Database()
+
+database.register(User)
 
 export const plugins = [
   VuexORM.install(database)
